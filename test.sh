a877a87d2c1f93615b1a58e428ba42eeecd0f1df
sudo-phantom <sudophantom@gmail.com>
2019-02-19 18:40:14 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -85,7 +85,7 @@ sleep 3;
 git clone https://github.com/1N3/Sn1per.git;
 cd Sn1per/;
 sh install.sh;
-yes;
+expect "[This script will install sniper under /usr/share/sniper. Are you sure you want to continue? (Hit Ctrl+C to exit)" {send "\r"};
 cd ~/tools;
 #wget https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-linux-x86_64.tar.gz;
 #tar -zxvf arachni-1.5.1-0.5.12-linux-x86_64.tar.gz;
