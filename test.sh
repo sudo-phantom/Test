8b1c0f79d1187c4305b6ab5d02296569aff7fd60
sudo-phantom <sudophantom@gmail.com>
2019-02-19 18:33:48 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -85,6 +85,7 @@ sleep 3;
 git clone https://github.com/1N3/Sn1per.git;
 cd Sn1per/;
 sh install.sh;
+yes;
 cd ~/tools;
 #wget https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-linux-x86_64.tar.gz;
 #tar -zxvf arachni-1.5.1-0.5.12-linux-x86_64.tar.gz;
